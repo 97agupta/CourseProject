2fbcd55abab28a4c4eb234e0fecc12a21f0a79dd
Sandeep Venkata <vsc5@illinois.edu>
2020-12-13 15:57:20 -0800

    Read combined_priors and filter only for words present on that day.

--- plsa_with_prior.py
+++ plsa_with_prior.py
@@ -1,13 +1,15 @@
 import csv
 import os
+import numpy as np
 
 from prior_plsa import Corpus, Pipeline
 from prior_plsa.pipeline import DEFAULT_PIPELINE
 from prior_plsa.algorithms import PriorPLSA
 
+from numpy import ndarray
 
-# Directory that contains the corpus data
 def plsa_with_prior_run():
+    # Directory that contains the corpus data
     directory = 'data'
 
     with open('plsa_with_prior.csv', mode='w') as file:
@@ -33,7 +35,10 @@ def plsa_with_prior_run():
 
             # Run PriorPLSA
             n_topics = 5
-            prior_plsa = PriorPLSA(corpus, n_topics, True)
+
+            prior: ndarray = load_priors(n_topics, corpus)
+
+            prior_plsa = PriorPLSA(corpus, n_topics, True, mu=30, prior_topic=prior)
 
             # Fit a PLSA model
             # result = prior_plsa.fit()
@@ -49,3 +54,27 @@ def plsa_with_prior_run():
             print(subdir)
             print(tuples)
 
+def load_priors(n_topics: int, corpus: Corpus) -> ndarray:
+    prior: ndarray = np.zeros((n_topics, corpus.n_words))
+    prev_topic: str = None
+    topic_id: int = -1
+
+    with open('prior_csvs\\combined_priors.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        next(csv_reader) # skip header
+
+        for row in csv_reader:
+            topic = row[3]
+            if prev_topic != topic:
+                prev_topic = topic
+                topic_id += 1
+
+            word = row[1]
+            probability = row[2]
+            if word in corpus.index:
+                wordIndex = corpus.index[word]
+                prior[topic_id][wordIndex] = float(probability)
+
+    #print(prior)
+
+# plsa_with_prior_run()
